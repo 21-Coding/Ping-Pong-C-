256caa43e65fe7326ad6022721d81c45e9f189ee
DominiqueYoumans <ddyoumans@icloud.com>
2020-02-22 15:04:02 -0800

    add initial repo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pingPong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user, enter a number!");
+        }
+    }
+}
