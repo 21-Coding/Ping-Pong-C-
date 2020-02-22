1da8a16e4d794e1172e82b10ecb1608a5fbf3cb5
DominiqueYoumans <ddyoumans@icloud.com>
2020-02-22 15:21:06 -0800

    add declaration of new array

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace pingPong
     {
         static void Main(string[] args)
         {
+            int[] numbers;
+
             Console.WriteLine("Hello user, enter a number!");
+            string stringUserNumber = Console.ReadLine();
+            int intUserNumber = int.Parse(stringUserNumber);
+
         }
     }
 }
