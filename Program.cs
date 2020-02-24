b040d4ff4181bf5e1b886d07b82d71268fe3d56b
DominiqueYoumans <ddyoumans@icloud.com>
2020-02-23 19:22:35 -0800

    add working ping pong code with text color

--- Program.cs
+++ Program.cs
@@ -1,22 +1,32 @@
-﻿using System;
-
-namespace pingPong
+using System;
+class  PingPong
 {
-    class Program
+  static void Main()
+  {
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("Hello! Welcome to Ping Pong, please enter a number:");
+    int userInput = int.Parse(Console.ReadLine());
+    for (int i = 1; i <= userInput; i++)
     {
-        static void Main(string[] args)
-        {
-
-            Console.WriteLine("Hello and welcome to Ping-Pong, enter a number!");
-            for (int index = 1; index < gameLingo.length; index++)
-            {
-              Console.WriteLine(gameLingo[index]);
-            }
-
-
-            string stringUserNumber = Console.ReadLine();
-            int intUserNumber = int.Parse(stringUserNumber);
-
-        }
+      if (i % 3 == 0 && i % 5 == 0)
+      {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Ping-Pong");
+      }
+      else if (i % 3 == 0)
+      {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Ping");
+      }
+      else if (i % 5 == 0)
+      {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Pong");
+      }
+      else
+      {
+        Console.WriteLine(i);
+      }
     }
+  }
 }
